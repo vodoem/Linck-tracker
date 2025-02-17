e00738d2c7a65d03fb9694499eb277e3596598c9
vodoem <prosto_duz@mail.ru>
2025-02-17 23:41:26 +0300

    Добавил логирование для отслеживания заголовков отправляющихся на гитхаб апи

--- bot/src/main/java/backend/academy/bot/client/GitHubClient.java
+++ bot/src/main/java/backend/academy/bot/client/GitHubClient.java
@@ -1,6 +1,10 @@
 package backend.academy.bot.client;
 
+import backend.academy.bot.TelegramPollingService;
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpEntity;
@@ -8,10 +12,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
+@Slf4j
 @Component
 public class GitHubClient {
     private final RestTemplate restTemplate;
     private final String baseUrl;
+    private static final Logger logger = LoggerFactory.getLogger(GitHubClient.class);
 
     public GitHubClient() {
         this.restTemplate = new RestTemplate();
@@ -23,6 +29,7 @@ public class GitHubClient {
         String url = baseUrl + "/repos/" + owner + "/" + repo;
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + System.getenv("GITHUB_TOKEN"));
+        logger.info(headers.toString());
 
         HttpEntity<Void> entity = new HttpEntity<>(headers);
 
